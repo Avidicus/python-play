121e9d11cb7b597256d115ae5a9678c88cb51002
Dan <avidicus@gmail.com>
2016-10-01 14:22:14 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+
+def main():
+
+    pass
+
+def setup():
+
+    pass
+
+def graph():
+
+    pass
+
+def firstDerivitaveFunction():
+
+    pass
+
+def firstDerivitaveApprox():
+
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())
